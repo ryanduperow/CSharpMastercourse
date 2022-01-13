662b997fa14bfd81d7daf4affd5de44aa744abef
RyanD <rkdupe@gmail.com>
2022-01-13 06:28:43 -0500

    first commit

---
+++ MyFirstProject/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// This is hoow you use the Console.Writeline method
+// Also note that you can use the "cw" snippet to create the Console.Writeline method signature
+Console.WriteLine("Hello, World!");
+Console.WriteLine("How are you?");
+
+Console.ReadLine();
